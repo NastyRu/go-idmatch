6fd554aa12ec2cb016791c7ac9cb2f8cc5a1be29
sag17u762 <nasty.sidenko@mail.ru>
2020-02-01 23:51:26 +0300

    Added command line arguments

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"github.com/maddevsio/go-idmatch/ocr"
 	"fmt"
 	"C"
+	"os"
 )
 
 //export RecognizeFrame
 func RecognizeFrame(name, folder string) {
-	_, p := ocr.Recognize(name, "" ,"" , folder)
+	fmt.Printf("Name: %s\n", name)
+	p := ocr.Recognize(name, "" ,"" , folder)
 	fmt.Printf("Path: %s\n", p)
 }
 
 func main() {
-	RecognizeFrame("/Users/anastasia/Desktop/working/id-card-detector/k.jpg", "/Users/anastasia/folder")
+	RecognizeFrame(os.Args[1], os.Args[2])
 }
